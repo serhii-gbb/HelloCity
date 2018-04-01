923260b4cfef63d441b24881eda61634aadae20d
serhii shevchenko <serhii.shevchenko23@gmail.com>
2018-04-01 12:40:01 +0300

    zone defining algorithm fixes

--- src/main/java/com/city/hello/jar_args_package/TimeZoneJarDefiner.java
+++ src/main/java/com/city/hello/jar_args_package/TimeZoneJarDefiner.java
@@ -3,19 +3,21 @@ package com.city.hello.jar_args_package;
 import com.city.hello.TimeZoneDefiner;
 
 import java.time.ZoneId;
+import java.util.Arrays;
 
 public class TimeZoneJarDefiner {
 
 
-
     public static ZoneId getTimeZone(String city, String timeZone) {
         String formatCity = city.replaceAll("\\s", "_");
-        for (String loopZone : ZoneId.getAvailableZoneIds()) {
 
-            if (timeZone.isEmpty() && loopZone.contains(formatCity)) {
+        for (String loopZone : ZoneId.getAvailableZoneIds()) {
+            String[] splitZone = loopZone.split("/");
+
+            if (timeZone.isEmpty() && Arrays.stream(splitZone).anyMatch(s-> s.equals(formatCity))) {
                 return ZoneId.of(loopZone);
 
-            } else if (!timeZone.isEmpty() && loopZone.contains(timeZone)) {
+            } else if (loopZone.equals(timeZone)) {
                 return ZoneId.of(loopZone);
 
             }
